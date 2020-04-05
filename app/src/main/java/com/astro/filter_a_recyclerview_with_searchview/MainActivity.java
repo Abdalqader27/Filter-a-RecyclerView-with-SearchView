06d0b12402ad2fbae0308cf2ab117fcd733b50f0
Abdalqader27 <46373332+Abdalqader27@users.noreply.github.com>
2020-04-05 15:24:49 +0300

    first

---
+++ app/src/main/java/com/astro/filter_a_recyclerview_with_searchview/MainActivity.java
@@ -0,0 +1,76 @@
+package com.astro.filter_a_recyclerview_with_searchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ExampleAdapter adapter;
+    private List<ExampleItem> exampleList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fillExampleList();
+        setUpRecyclerView();
+    }
+
+    private void fillExampleList() {
+        exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "One", "Ten"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Two", "Eleven"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Three", "Twelve"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Four", "Thirteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Five", "Fourteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Six", "Fifteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Seven", "Sixteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Eight", "Seventeen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_ac_unit_black_24dp, "Nine", "Eighteen"));
+    }
+
+    private void setUpRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        adapter = new ExampleAdapter(exampleList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+}
